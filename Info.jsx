e55e9e6f1591650c520c1a39f4d3d0a229f99baa
rabiha-tull-islam <71832627+rabiha-tull-islam@users.noreply.github.com>
2021-06-26 23:37:51 +0500

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>Basic web dev reat js Bootcamps</p>
+    </div>
+  );
+}
+export default Info;
